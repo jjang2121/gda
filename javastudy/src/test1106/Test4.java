f4c865cb99f70e9658039c77f2ae886372b6faf6
gd <gd@DESKTOP-N3BM6OR>
2023-11-06 17:46:25 +0900

    HomeworkEnd_231106_1746

--- javastudy/src/test1106/Test4.java
+++ javastudy/src/test1106/Test4.java
@@ -13,13 +13,16 @@ interface LambdaInterface3 {
 public class Test4 {
 	public static void main(String[] args) {
 		LambdaInterface3 f;
-		//식 미리 정의
+		//두수의 합
 		f = (x, y) -> x + y;
 		System.out.println("두수 (5,2)의 합:" + calc(5,2,f));//7
+
+		//두수의 곱
 		f = (x, y) -> x * y;
 		System.out.println("두수 (5,2)의 곱:" + calc(5,2,f));//10
 
 		int[] arr = {1,2,3,4,5,6,7,8,9,10};
+		//배열 최대값
 		f = (x, y) -> {
 			int maxNum = 0;
 			for(int i=0;i<arr.length;i++) {
@@ -30,6 +33,8 @@ public class Test4 {
 			return maxNum;
 		};
 		System.out.println("arr 배열의 최대값:" + calc(arr,f));//10
+
+		//배열 최소값
 		f = (x, y) -> {
 			int minNum = 0;
 			for(int i=0;i<arr.length;i++) {
